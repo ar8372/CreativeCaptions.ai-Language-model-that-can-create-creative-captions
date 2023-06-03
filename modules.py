093e50eb4582a5c167e7b320288b0e67733c2c67
ar8372 <ar837232342@gmail.com>
2023-06-03 22:05:26 +0530

    baseline

---
+++ modules.py
@@ -0,0 +1,29 @@
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse, HTMLResponse, RedirectResponse
+from pydantic import BaseModel
+import io
+import json
+import requests
+from transformers import VisionEncoderDecoderModel, ViTFeatureExtractor, AutoTokenizer
+import torch
+from PIL import Image
+
+# Preliminaries
+import os
+import numpy as np 
+import pandas as pd
+
+#transformers
+from transformers import GPT2LMHeadModel
+from transformers import GPT2Tokenizer
+
+# Pytorch
+import torch
+import torch.nn as nn
+
+#warnings
+import warnings
+warnings.filterwarnings('ignore')
+
+# My Module
+#import config
